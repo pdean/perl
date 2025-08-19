d7c41e43528da840fa79fb911354fb960e0aa800
peter dean <p.dean@internode.on.net>
2025-08-19 18:45:15 +1000

    perltidy

--- avws/avws.pl
+++ avws/avws.pl
@@ -5,26 +5,25 @@ use v5.36;
 use aliased 'Geo::Proj::CCT';
 use POSIX qw(copysign);
 
-my $cs1 = CCT->crs2crs( "EPSG:7856" , "EPSG:7844" );
+my $cs1 = CCT->crs2crs( "EPSG:7856", "EPSG:7844" );
 $cs1 = $cs1->norm;
 
 my $S = " +proj=pipeline  +zone=56 +south +ellps=GRS80";
 $S .= " +step +inv +proj=utm";
-#$S .= " +step +proj=vgridshift +grids=au_ga_AUSGeoid2020_20180201.tif";
 $S .= " +step +proj=vgridshift +grids=au_ga_AGQG_20201120.tif";
 $S .= " +step +proj=utm";
 my $cs2 = CCT->create($S);
 
-my $lat1 = minutes(-23, 27);
-my $lon1 = minutes(149, 54);
-my $lat2 = minutes(-22, 30);
-my $lon2 = minutes(150, 51);
+my $lat1 = minutes( -23, 27 );
+my $lon1 = minutes( 149, 54 );
+my $lat2 = minutes( -22, 30 );
+my $lon2 = minutes( 150, 51 );
 
-for (my $latm = $lat1; $latm <= $lat2; $latm++) {
-    for (my $lonm = $lon1; $lonm <= $lon2; $lonm++) {
-        my $lat = $latm/60.0;
-        my $lon = $lonm/60.0;
-        my $posd = [$lon, $lat, 0.0];
+for ( my $latm = $lat1; $latm <= $lat2; $latm++ ) {
+    for ( my $lonm = $lon1; $lonm <= $lon2; $lonm++ ) {
+        my $lat  = $latm / 60.0;
+        my $lon  = $lonm / 60.0;
+        my $posd = [ $lon, $lat, 0.0 ];
         my $posm = $cs1->inv($posd);
         my $posa = $cs2->fwd($posm);
         printf "%.3f,%.3f,%.3f\n", @$posa;
@@ -36,5 +35,4 @@ sub minutes {
     $min = copysign( $min, $deg );
     return $deg * 60 + $min;
 }
-
 
